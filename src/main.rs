82c34844999ea65857be9701196e69596eadd458
MacBobby Chibuzor <theghostmac@gmail.com>
2023-05-28 10:51:56 +0100

    completed the web server

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::net::TcpListener;
 
-
 fn main() {
-    // create a listener similar to a new server mux.
-    let serverListener = TcpListener::bind("127.0.0.1:5000")
+    // create a listener and bind to a port.
+    let server_listener = TcpListener::bind("127.0.0.1:5000")
         .unwrap();
 
-    // listen to incoming streams from TcpListener
-    for incomingStreams in serverListener.incoming() {
-        let incomingStreams = incomingStreams.unwrap();
-
+    // listen to incoming streams from TcpListener.
+    for incoming_streams in server_listener.incoming() {
+        // calling unwrap to terminate the server when there is an error.
+        let _incoming_streams = incoming_streams.unwrap();
+        // server message...
         println!("Successful connection to server...")
     }
 }
